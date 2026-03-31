fa9e8d4bfb5875203c344ced6e3442dbce2494df
Rinku Diwakar <prateek.diwakarr@gmail.com>
2026-03-31 15:16:23 +0530

    added the project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This si my first capstone project which is end to end mlops project',
+    author='Rinku Diwakar',
+    license='MIT',
+)
