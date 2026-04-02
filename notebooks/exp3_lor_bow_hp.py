17ea0c5284e0db1fcd080660495d7a0e51fab5e4
Rinku Diwakar <prateek.diwakarr@gmail.com>
2026-04-02 18:02:55 +0530

    set the exp 3 and run the file

--- notebooks/exp3_lor_bow_hp.py
+++ notebooks/exp3_lor_bow_hp.py
@@ -21,8 +21,8 @@ warnings.filterwarnings("ignore")
 # os.environ["MLFLOW_DISABLE_ARTIFACTS_DOWNLOAD"] = "1"
 
 # Set MLflow Tracking URI & DAGsHub integration
-MLFLOW_TRACKING_URI = "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow"
-dagshub.init(repo_owner="vikashdas770", repo_name="YT-Capstone-Project", mlflow=True)
+MLFLOW_TRACKING_URI = "https://dagshub.com/rinkudiwakar/capstone-Project-1.mlflow"
+dagshub.init(repo_owner="rinkudiwakar", repo_name="capstone-Project-1", mlflow=True)
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment("LoR Hyperparameter Tuning")
 
